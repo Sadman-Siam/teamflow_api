630e217b4db0b00967f6584b66f3aa3fe6db4a70
Siam Sadman <sadman.bin.anwar.siam@gmail.com>
2025-08-03 01:53:17 +0600

    env and routes

---
+++ api/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+const PORT = 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+//routes
+const userRoutes = require("./routes/userRoutes");
+app.use("/users", userRoutes);
+
+//test route
+router.get("/", (req, res) => {
+  res.status(200).json({
+    message: "API is running",
+    welcome: "Welcome to the Teamflow API",
+  });
+});
+
+// Use the router
+app.use("/api", router);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT: ${PORT}`);
+});
