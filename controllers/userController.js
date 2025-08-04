54fec0ca4eb28fc229ae25ae0247058ad70eb322
Siam Sadman <sadman.bin.anwar.siam@gmail.com>
2025-08-04 20:33:34 +0600

    update controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,29 +1,61 @@
-// Placeholder controller functions
-const getUser = (req, res) => {
-  res.status(200).json({
-    message: "Get user endpoint",
-    data: [],
-  });
+const userService = require("../services/userService");
+
+const getUser = async (req, res) => {
+  const query = req.query;
+  const result = await userService.getUser(query);
+  if (result.success) {
+    return res.status(200).json({
+      data: result.data,
+    });
+  } else {
+    return res.status(404).json({
+      message: result.message,
+    });
+  }
 };
 
-const createUser = (req, res) => {
-  res.status(201).json({
-    message: "Create user endpoint",
-    data: req.body,
-  });
+const createUser = async (req, res) => {
+  const userData = req.body;
+  const result = await userService.createUser(userData);
+  if (result.success) {
+    return res.status(201).json({
+      message: result.message,
+    });
+  } else {
+    return res.status(500).json({
+      message: result.message,
+    });
+  }
 };
 
-const updateUser = (req, res) => {
-  res.status(200).json({
-    message: "Update user endpoint",
-    data: req.body,
-  });
+const updateUser = async (req, res) => {
+  const query = req.query;
+  const data = req.body;
+  const result = await userService.updateUser(query, data);
+  if (result.success) {
+    return res.status(200).json({
+      message: result.message,
+      data: result.data,
+    });
+  } else {
+    return res.status(404).json({
+      message: result.message,
+    });
+  }
 };
 
-const deleteUser = (req, res) => {
-  res.status(200).json({
-    message: "Delete user endpoint",
-  });
+const deleteUser = async (req, res) => {
+  const query = req.query;
+  const result = await userService.deleteUser(query);
+  if (result.success) {
+    return res.status(200).json({
+      message: result.message,
+    });
+  } else {
+    return res.status(404).json({
+      message: result.message,
+    });
+  }
 };
 
 module.exports = {
